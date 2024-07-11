d647900e631b74972eb3064b3533629310dbfa77
Rishitha-CSE <108116440+Rishitha-CSE@users.noreply.github.com>
2024-07-12 01:10:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed=new typed(".text",{
+    Strings:["Computer Science","Data Science Enthusiast","Programmer","Full Stack Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:10000,
+    loop:true
+});
